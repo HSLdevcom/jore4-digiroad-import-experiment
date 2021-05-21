9c2b830791fc97582016e4bb3880bea8695f64d6
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-05-21 20:51:02 +0300

    WIP Add speed limits into database dump.

--- export_pgdump.sh
+++ export_pgdump.sh
@@ -13,7 +13,7 @@ docker start $DOCKER_CONTAINER
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres $DOCKER_IMAGE sh -c "$PG_WAIT"
 
 PGDUMP_OUTPUT="digiroad_k_$(date "+%Y-%m-%d").pgdump"
-OUTPUT_TABLES="dr_link_id dr_linkki_k dr_pysakki dr_kaantymisrajoitus_k"
+OUTPUT_TABLES="dr_link_id dr_linkki_k dr_pysakki dr_kaantymisrajoitus_k dr_nopeusrajoitus_k"
 OUTPUT_TABLE_OPTIONS="`echo ${OUTPUT_TABLES[@]} | sed \"s/dr_/-t ${DB_IMPORT_SCHEMA_NAME}.dr_/g\"`"
 
 mkdir -p ${WORK_DIR}/pgdump
