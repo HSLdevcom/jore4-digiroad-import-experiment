6932f7928c4bec8c09f0e1b33bfeccfdeaeee1e7
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-08-20 16:27:45 +0300

    Print commands to stdout while executing shell scripts.

--- import_digiroad_links.sh
+++ import_digiroad_links.sh
@@ -9,6 +9,9 @@ if [[ "$#" -ne 0 && "$#" -ne 4 && "$#" -ne 5 ]]; then
     exit 1
 fi
 
+# Stop on first error. Have meaningful error messages. Print each command to stdout.
+set -euxo pipefail
+
 DB_HOST="${1:-localhost}"
 DB_PORT="${2:-5432}"
 DB_NAME="${3:-digiroad}"
@@ -16,9 +19,6 @@ DB_USER="${4:-digiroad}"
 MIGRATE_TO_HASURA=${5:false}
 
 PGDUMP_DIR="$(pwd)/workdir/pgdump"
-
-# Stop on first error.
-set -eu
 
 # Find pg_dump file having date suffix for current day.
 PGDUMP_FILE_PATTERN="digiroad_k_$(date "+%Y-%m-%d").pgdump"
@@ -34,14 +34,14 @@ SCHEMA_NAME="digiroad"
 # Create schema into target database. Schema name must match with what is used in the pg_dump file.
 psql -v ON_ERROR_STOP=1 -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -b -c "CREATE SCHEMA IF NOT EXISTS ${SCHEMA_NAME};"
 
-echo "Restoring Digiroad links from ${PGDUMP_FILE}"
+# Restore Digiroad links from pg_dump file.
 
 pg_restore -v -Fc -j8 -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" \
  --no-security-labels --no-owner --no-privileges --clean --if-exists "${PGDUMP_FILE}"
 
 if [[ ${MIGRATE_TO_HASURA} == "true" ]]; then
-    echo "Migrating Digiroad links to Hasura/SQL schema."
 
+    # Migrate Digiroad links to Hasura/SQL schema.
     psql --set=AUTOCOMMIT=off -v ON_ERROR_STOP=1 -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" <<-EOSQL
         BEGIN;
 
