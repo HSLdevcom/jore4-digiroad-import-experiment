ea8790971192d02c1938b2096817676e100a1ff6
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-09-29 15:05:26 +0300

    Add more not-null constraints into `dr_pysakki` table.

--- sql/add_dr_pysakki_constraints.sql
+++ sql/add_dr_pysakki_constraints.sql
@@ -1,9 +1,12 @@
 -- Add data integrity constraints.
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN gid SET NOT NULL;
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN link_id SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN valtak_id SET NOT NULL;
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN kuntakoodi SET NOT NULL;
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN koord_x SET NOT NULL;
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN koord_y SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN sijainti_m SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN vaik_suunt SET NOT NULL;
 ALTER TABLE :schema.dr_pysakki ALTER COLUMN geom SET NOT NULL;
 
 ALTER TABLE :schema.dr_pysakki ADD CONSTRAINT dr_pysakki_pkey PRIMARY KEY (gid);
--- sql/create_mbtiles_schema.sql
+++ sql/create_mbtiles_schema.sql
@@ -41,4 +41,12 @@ ALTER TABLE :schema.dr_linkki ADD CONSTRAINT dr_linkki_pkey PRIMARY KEY (gid);
 ALTER TABLE :schema.dr_linkki ADD CONSTRAINT uk_dr_linkki_link_id UNIQUE (link_id);
 
 -- Add data integrity constraints to dr_pysakki table.
-\i :sql_dir/add_dr_pysakki_constraints.sql
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN gid SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN link_id SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN kuntakoodi SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN koord_x SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN koord_y SET NOT NULL;
+ALTER TABLE :schema.dr_pysakki ALTER COLUMN geom SET NOT NULL;
+
+ALTER TABLE :schema.dr_pysakki ADD CONSTRAINT dr_pysakki_pkey PRIMARY KEY (gid);
+ALTER TABLE :schema.dr_pysakki ADD CONSTRAINT fk_dr_pysakki_link_id FOREIGN KEY (link_id) REFERENCES :schema.dr_linkki (link_id);
