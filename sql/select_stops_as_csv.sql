18d4ef8b2035f91ffb4566fe58557d5618278adf
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-10-21 12:12:10 +0300

    Review changes
    - Do not use the term scheduled stop point.
    - Export coordinates in GEOJSON format by using the coordinate
      system EPSG:4326 (WGS84).
    - Add finnish and swedish names to the export file.

--- sql/select_stops_as_csv.sql
+++ sql/select_stops_as_csv.sql
@@ -1,14 +1,15 @@
 COPY (
     SELECT
-        source.gid as external_stop_point_id,
+        source.gid as external_stop_id,
         source.link_id as external_link_id,
-        source.valtak_id as national_stop_point_id,
+        source.valtak_id as national_stop_id,
         CASE
             WHEN source.vaik_suunt = 2 THEN 'forward'
             WHEN source.vaik_suunt = 3 THEN 'backward'
         END AS direction_on_infra_link,
-        source.koord_x as coordinate_x,
-        source.koord_y as coordinate_y,
-        'digiroad_r' as external_stop_point_source
+        ST_AsGeoJSON(ST_Transform(source.geom, 4326))::jsonb as location,
+        source.nimi_su as finnish_name,
+        source.nimi_ru as swedish_name,
+        'digiroad_r' as external_stop_source
     FROM :schema.dr_pysakki source
 ) TO STDOUT WITH (FORMAT CSV, HEADER)
