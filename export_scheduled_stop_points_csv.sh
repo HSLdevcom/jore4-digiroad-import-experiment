4a927b76053e58a4eba54d057a3de8f08be0be22
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-10-21 09:48:45 +0300

    Export scheduled stop points to a CSV file

---
+++ export_scheduled_stop_points_csv.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Stop on first error. Have meaningful error messages. Print each command to stdout.
+set -euxo pipefail
+
+# Source common environment variables.
+source "$(cd "$(dirname "$0")"; pwd -P)/set_env_vars.sh"
+
+# Start Docker container. The container is expected to exist and contain all the data to be exported.
+docker start $DOCKER_CONTAINER
+
+# Wait for PostgreSQL to start.
+docker exec "${DOCKER_CONTAINER}" sh -c "$PG_WAIT_LOCAL"
+
+# Export csv file to output directory.
+OUTPUT_FILENAME="scheduled_stop_points.csv"
+mkdir -p ${WORK_DIR}/jore4_scheduled_stop_points
+docker run --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql -v ${CWD}/workdir/jore4_scheduled_stop_points:/tmp/jore4_scheduled_stop_points ${DOCKER_IMAGE} \
+  sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/select_scheduled_stop_points_as_csv.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -o /tmp/jore4_scheduled_stop_points/${OUTPUT_FILENAME}"
+
+# Stop Docker container.
+docker stop $DOCKER_CONTAINER
