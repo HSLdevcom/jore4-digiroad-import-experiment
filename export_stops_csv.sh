ecf5cad99e122c37ddbc7b47d038455b89ce01bf
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-10-21 12:12:10 +0300

    Review changes
    - Do not use the term scheduled stop point.
    - Export coordinates in GEOJSON format by using the coordinate
      system EPSG:4326 (WGS84).
    - Add finnish and swedish names to the export file.

--- export_stops_csv.sh
+++ export_stops_csv.sh
@@ -13,10 +13,10 @@ docker start $DOCKER_CONTAINER
 docker exec "${DOCKER_CONTAINER}" sh -c "$PG_WAIT_LOCAL"
 
 # Export csv file to output directory.
-OUTPUT_FILENAME="scheduled_stop_points.csv"
-mkdir -p ${WORK_DIR}/jore4_scheduled_stop_points
-docker run --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql -v ${CWD}/workdir/jore4_scheduled_stop_points:/tmp/jore4_scheduled_stop_points ${DOCKER_IMAGE} \
-  sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/select_scheduled_stop_points_as_csv.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -o /tmp/jore4_scheduled_stop_points/${OUTPUT_FILENAME}"
+OUTPUT_FILENAME="digiroad_stops.csv"
+mkdir -p ${WORK_DIR}/csv
+docker run --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql -v ${CWD}/workdir/csv:/tmp/csv ${DOCKER_IMAGE} \
+  sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/select_stops_as_csv.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -o /tmp/csv/${OUTPUT_FILENAME}"
 
 # Stop Docker container.
 docker stop $DOCKER_CONTAINER
