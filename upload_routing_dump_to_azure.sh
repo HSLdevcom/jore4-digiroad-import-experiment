707bcfbc9fb09c48ee9294a969e4e188b1a9613d
Jörg Schummer <jorg.schummer@vincit.fi>
2022-02-18 15:55:21 +0200

    Add warning to upload script about potential azcli version issue

--- upload_routing_dump_to_azure.sh
+++ upload_routing_dump_to_azure.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Azure CLI is required to be installed.
+# NB: Not all versions of Azure CLI seem to work for uploading the files, see the error message below for more info.
 
 # The target file is an SQL dump file for routing schema for current date.
 SQL_FILE="./workdir/pgdump/digiroad_r_routing_$(date "+%Y-%m-%d").sql"
@@ -15,12 +16,22 @@ az login
 az account set --subscription "jore4"
 
 # Upload dump file to Azure Blob Storage.
-time az storage azcopy blob upload \
+if ! time az storage azcopy blob upload \
   --source "${SQL_FILE}" \
   --recursive \
   --account-name "jore4storage" \
   --auth-mode "login" \
-  --container "jore4-digiroad"
+  --container "jore4-digiroad"; then
+    cat <<EOF
+
+Upload failed. Please make sure that you have installed a version of Azure CLI, which is known to work
+(e.g. 2.29.2, may require downgrade).
+
+Alternatively you may perform the upload manually in the azure portal (storage account "jore4storage", container
+"jore4-digiroad").
+EOF
+    exit 2
+fi
 
 # List all artifacts inside `jore4-digiroad` in blob storage container.
 az storage blob list \
