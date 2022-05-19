680180be21eadcbba102156000ef870deb6f51c9
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-05-19 14:46:11 +0300

    Refactor GeoPackage file for QGIS fixup.
    GeoPackage layer names are changed: 'fixup' -> 'add_link', 'removed' -> 'remove_link'
    
    'link_id' column type is changed to integer and values changed to start from 1.
    
    New columns are added for vehicle modes/types.
    
    Geometry columns are renamed to `geom` (GeoPackage default).

--- import_digiroad_shapefiles.sh
+++ import_digiroad_shapefiles.sh
@@ -70,15 +70,15 @@ do
       sh -c "for SUB_AREA in ${SUB_AREAS}; do $SHP2PGSQL -a /tmp/shp/\${SUB_AREA}/${SUB_AREA_SHP_TYPE}.shp $TABLE_NAME | $PSQL -v ON_ERROR_STOP=1; done"
 done
 
-# Import "fixup" and "removed" layers from GeoPackage fixup file if it exists.
+# Import "add_link" and "remove_link" layers from GeoPackage fixup file if it exists.
 if [ -f fixup/digiroad/fixup.gpkg ]; then
   OGR2OGR="exec ogr2ogr -f PostgreSQL \"PG:host=\$POSTGRES_PORT_5432_TCP_ADDR port=\$POSTGRES_PORT_5432_TCP_PORT dbname=$DB_NAME user=digiroad schemas=$DB_IMPORT_SCHEMA_NAME\""
 
   docker run --rm --link "${DOCKER_CONTAINER_NAME}":postgres -v ${CWD}/fixup/digiroad:/tmp/gpkg $DOCKER_IMAGE \
-    sh -c "$OGR2OGR /tmp/gpkg/fixup.gpkg -nln fix_layer_link fixup"
+    sh -c "$OGR2OGR /tmp/gpkg/fixup.gpkg -nln fix_layer_link add_link"
 
   docker run --rm --link "${DOCKER_CONTAINER_NAME}":postgres -v ${CWD}/fixup/digiroad:/tmp/gpkg $DOCKER_IMAGE \
-    sh -c "$OGR2OGR /tmp/gpkg/fixup.gpkg -nln fix_layer_link_exclusion_geometry removed"
+    sh -c "$OGR2OGR /tmp/gpkg/fixup.gpkg -nln fix_layer_link_exclusion_geometry remove_link"
 fi
 
 # Load DR_PYSAKKI shapefile into database.
